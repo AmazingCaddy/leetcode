46e74b9d6338cbb55b072932699a3c2096b98522
Wenbin Wang <ecnuwbwang@gmail.com>
2013-12-25 21:15:25 +0800

    add Longest_Common_Prefix.cpp

---
+++ Longest_Common_Prefix.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	void test() {
+		std::vector<string> v;
+		v.push_back("abcdef");
+		v.push_back("abcde");
+		v.push_back("abcdefghi");
+		cout << this->longestCommonPrefix(v) << "\n";
+	}
+	string longestCommonPrefix(vector<string> &strs) {
+		int ans = 0;
+		string res = "";
+		if (strs.size() == 0) {
+			return res;
+		}
+		while (1) {
+			if (strs[0].size() <= ans) {
+				break;
+			}
+			char common = strs[0][ans];
+			int flag = 0;
+			for (int i = 1; i < strs.size(); i ++) {
+				if (strs[i].size() <= ans || strs[i][ans] != common) {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag) {
+				break;
+			}
+			res = res + common;
+			ans ++;
+		}
+		return res;
+	}
+};
+
+int main (int argc, char *argv[]) {
+	Solution *sol = new Solution();
+	sol->test();
+	delete sol;
+	return 0;
+}
