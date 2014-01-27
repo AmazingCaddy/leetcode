a2211c7f1862cb0308c3282dc4c653114049c2d3
Wenbin Wang <ecnuwbwang@gmail.com>
2014-01-27 14:51:10 +0800

    add Remove_Nth_Node_From_End_of_List.cpp

--- Remove_Nth_Node_From_End_of_List.cpp
+++ Remove_Nth_Node_From_End_of_List.cpp
@@ -1,13 +1,27 @@
+#define DEBUG
+
+#ifdef DEBUG
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
+
 struct ListNode {
 	int val;
 	ListNode *next;
 	ListNode(int x) : val(x), next(NULL) {}
 };
+#endif
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *	 int val;
+ *	 ListNode *next;
+ *	 ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 private:
 	int get_list_len (ListNode *head) {
@@ -47,7 +61,7 @@ private:
 
 	void print (ListNode * head) {
 		int cnt = 0;
-	//	ListNode *p = head;
+		ListNode *p = head;
 		while (head) {
 			if (cnt) {
 				cout << " -> " << head -> val;
@@ -58,7 +72,7 @@ private:
 			head = head -> next;
 		}
 		cout << "\n";
-	//	head = p;
+		head = p;
 	}
 
 	ListNode * reorder(ListNode *head) {
@@ -74,69 +88,42 @@ private:
 public:
 	void test () {
 		int list_a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		for (int i = 1; i <= 10; i ++) {
-			for (int k = 0; k <= 10; k ++) {
-				ListNode * la = this -> create(list_a, i);
-				this -> print(la);
-				la = this -> reverseKGroup(la, k);
-				this -> print(la);
-				this -> del(la);
-			}
+		for (int i = 6; i <= 10; i ++) {
+			ListNode * la = this -> create(list_a, i);
+			this -> print(la);
+			la = this -> removeNthFromEnd(la, 6);
+			this -> print(la);
+			this -> del(la);
 		}
 	}
 
-	ListNode *reverseKGroup(ListNode *head, int k) {
-		if (head == NULL || k <= 1) {
+	ListNode *removeNthFromEnd(ListNode *head, int n) {
+		int list_len = this -> get_list_len(head);
+		if (list_len < n) {
 			return head;
 		}
-		int list_len = this->get_list_len(head);
-		int multi = list_len / k;
-		vector<ListNode*> v;
-		vector<int> v_len;
-		ListNode *p = head, *q = head;
-		for (int i = 0; i < multi; i ++) {
-			v.push_back(p);
-			v_len.push_back(k);
-			for (int j = 0; j < k - 1; j ++) {
-		//		cout << p -> val << " ";
-				p = p -> next;
-			}
-		//	cout << p -> val << "\n";
-			q = p -> next;
-			p -> next = NULL;
-			p = q;
+		int k = list_len - n;
+		ListNode *p = head, *q;
+		if (!k) {
+			head = p -> next;
+			delete p;
+			return head;
 		}
-		if (p) {
-			v.push_back(p);
-			v_len.push_back(this->get_list_len(p));
+		for (int i = 0; i < k - 1; i ++) {
+			p = p -> next;
 		}
-		int sz = v.size();
-		//head = v[0];
-		//this -> print(head);
-		for (int i = 0; i < sz; i ++) {
-		//	this -> print(v[i]);
-			if (v_len[i] == k) {
-				v[i] = this -> reorder(v[i]);
-			}
-		//	this -> print(v[i]);
-			//cout << v_len[i] << "\n";
-		}
-		//head = v[0];
-		//this -> print(head);
-		for (int i = 0; i < sz - 1; i ++) {
-			ListNode *p = v[i];
-			while (p -> next) {
-				p = p -> next;
-			}
-			p -> next = v[i + 1];
-		}
-		return v[0];
+		q = p -> next;
+		p -> next = q -> next;
+		delete q;
+		return head;   
 	}
 };
 
+#ifdef DEBUG
 int main(int argc, char const *argv[]) {
 	Solution *sol = new Solution();
 	sol -> test();
-	delete sol;
+	delete sol;	
 	return 0;
 }
+#endif
