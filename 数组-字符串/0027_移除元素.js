2cd7063177ff1965c3904de9afce679c48a34b8a
Wenbin Wang <ecnuwbwang@gmail.com>
2024-01-14 22:14:12 +0800

    增加数组-字符串题目

---
+++ 数组-字符串/0027_移除元素.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  const n = nums.length;
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+      if (nums[i] !== val) {
+          nums[cnt] = nums[i];
+          cnt ++;
+      }
+  }
+  return cnt;
+};
